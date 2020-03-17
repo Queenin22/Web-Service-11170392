f334d6337db98797f5f2e49d2023c9ff5ceb267e
Queenin22 <intanhidayah122@gmail.com>
2020-03-17 10:57:44 +0700

    pengujian request

--- transaksi.php
+++ transaksi.php
@@ -1,11 +1,11 @@
 <?php
 include("Api.php");
-$barang = new Api();
+$transaksi = new Api();
 $data = [ 
      'id_transaksi' => $_GET["id_transaksi"],
 	 'id_barang' => $_GET["id_barang"],
 	 'id_pembeli' => $_GET["id_pembeli"],
 	 'tanggal'  => date('Y-m-d'),
 	 'keterangan' => $_GET['keterangan']];
-echo $barang->post("http://localhost/penjualan/submit.php",$data);
+echo $transaksi->post("http://localhost/penjualan/submit.php",$data);
 ?> 
